019c50f3ff2c4fb1d42da6841290cef49c9cc703
elicastillo0 <elicastillo92@yahoo.com>
2025-02-25 18:38:40 +0100

    add main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+let users = ["Juan Pérez", "Lucía Gómez", "Carlos López", "Elí Castillo"];
 
-const operation = (numero1, numero2, op) => {
-    return op(numero1, numero2)
+const getUser = (position, seconds) => {
+    return new Promise((resolve, reject) => {
+        if(position < 0 || position >= users.length) {
+            reject({mensaje: "Posición no valida"})
+        }
+        setTimeout(() => {
+           resolve({user: users[position]}) 
+        }, seconds * 1000);
+    })
 }
-operation(1, 3, (a, b) => a + b)
+
+getUser(2, 3)
+    .then(data => {
+        console.log(data);
+        return data.user;
+    })
+    .then(data => {
+        console.log("Hola " + data);
+    })
+    .catch(error => console.error(error));
